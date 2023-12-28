f1aa50df4936b0ff62c4c788063a4ebd81577b83
Iamneo <techadmin@iamneo.ai>
2023-12-28 05:14:02 +0000

    Auto-save-commit-12/28/2023--10:44:08-AM

--- dotnetapp1/Program.cs
+++ dotnetapp1/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EntityDb")));
 
 var app = builder.Build();
 
@@ -11,7 +11,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
