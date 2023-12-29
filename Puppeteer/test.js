e543128843177a2a34ea17511b40d89f5ca3da20
user <meiroopashree@gmail.com>
2023-12-29 07:23:06 +0000

    commit message

--- Puppeteer/test.js
+++ Puppeteer/test.js
@@ -1,3 +1,4 @@
+
 const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({
@@ -7,7 +8,7 @@ const puppeteer = require('puppeteer');
 
     const page1 = await browser.newPage();
     try{
-      await page1.goto('http://localhost:8080/Book/Create');
+      await page1.goto('https://8080-bfdeeddcedfabcfacbdcbaeadbebabcdebdca.premiumproject.examly.io/Book/Create');
       await page1.setViewport({
         width:1200,
         height:800,
@@ -17,32 +18,27 @@ const puppeteer = require('puppeteer');
       const genreInput = await page1.$('input[name="Genre"]');
       const priceInput = await page1.$('input[name="Price"]');
       const quantityInput = await page1.$('input[name="Quantity"]');
-      const createButton = await page1.$('input[type="submit"]');
       
-    //   console.log('\nTESTCASE: Create New Book Form Elements');
-      console.log(titleInput ? 'Title Input Exists: Success' : 'Title Input Missing: Failure');
-      console.log(authorInput ? 'Author Input Exists: Success' : 'Author Input Missing: Failure');
-      console.log(genreInput ? 'Genre Input Exists: Success' : 'Genre Input Missing: Failure');
-      console.log(priceInput ? 'Price Input Exists: Success' : 'Price Input Missing: Failure');
-      console.log(quantityInput ? 'Quantity Input Exists: Success' : 'Quantity Input Missing: Failure');
-      console.log(createButton ? 'Create Button Exists: Success' : 'Create Button Missing: Failure');
-      
-      console.log('TESTCASE:Create_Book_successful:success');
+ 
+      if (titleInput != null && authorInput!= null && genreInput!=null && priceInput!=null && quantityInput!=null)
+      console.log('TESTCASE:Create_Book_Input_successful:success');
+      else
+      console.log('TESTCASE:Create_Book_Input_successful:failure');
       
     }
     catch(e){
-      console.log('TESTCASE:Create_Book_successful:failure');
+      console.log('TESTCASE:Create_Book_Input_successful:failure');
     } 
 
     const page2 = await browser.newPage();
 
     try {
-      await page2.goto('http://localhost:8080');
+      await page2.goto('https://8080-bfdeeddcedfabcfacbdcbaeadbebabcdebdca.premiumproject.examly.io');
       await page2.setViewport({
         width: 1200,
         height: 800,
       });
-      await page2.goto('http://localhost:8080/Book/Details/1');
+      await page2.goto('https://8080-bfdeeddcedfabcfacbdcbaeadbebabcdebdca.premiumproject.examly.io/Book/Details/1');
     
       const bookDetailsExist = await page2.evaluate(() => {
       const bookId = document.querySelector('dd[data-book="BookID"]');
@@ -54,39 +50,26 @@ const puppeteer = require('puppeteer');
       
       return bookId && title && author && genre && price && quantity;
     });
-
-    console.log('TESTCASE:Book_Details_successful:success');  
+    console.log('TESTCASE:Book_Details_exist_successful:success');
     }
   catch(e){
-    console.log('TESTCASE:Book_Details_successful:success');
+    console.log('TESTCASE:Book_Details_exist_successful:failure');
   }
-    
-  const page3 = await browser.newPage();
 
-  try {
-    await page3.goto('http://localhost:8080/Book/Index');
-    
-    const titleColumn = await page3.$('th:contains("Title")');
-    const authorColumn = await page3.$('th:contains("Author")');
-    const genreColumn = await page3.$('th:contains("Genre")');
-    const priceColumn = await page3.$('th:contains("Price")');
-    const quantityColumn = await page3.$('th:contains("Quantity")');
-    
-    console.log(titleColumn ? 'TESTCASE:BookId_Column_successful:success' : 'TESTCASE:BookId_Column_failure:failure');
-    console.log(authorColumn ? 'TESTCASE:Author_Column_successful:success' : 'TESTCASE:Author_Column_failure:failure');
-    console.log(genreColumn ? 'TESTCASE:Genre_Column_successful:success' : 'TESTCASE:Genre_Column_failure:failure');
-    console.log(priceColumn ? 'TESTCASE:Price_Column_successful:success' : 'TESTCASE:Price_Column_failure:failure');
-    console.log(quantityColumn ? 'TESTCASE:Quantity_Column_successful:success' : 'TESTCASE:Quantity_Column_failure:failure');
-    
-  } 
-  catch(e){
-    console.log('TESTCASE:BookId_Column_failure:failure');
-    console.log('TESTCASE:Author_Column_failure:failure');
-    console.log('TESTCASE:Genre_Column_failure:failure');
-    console.log('TESTCASE:Price_Column_failure:failure');
-    console.log('TESTCASE:Quantity_Column_failure:failure');
+  const page3 = await browser.newPage();
+  try
+  {
+    await page3.goto('https://8080-bfdeeddcedfabcfacbdcbaeadbebabcdebdca.premiumproject.examly.io/Book/Create');
+
+    await Promise.all([
+      page3.waitForNavigation(),
+      page3.click('a[href="/"]'),
+    ]);
+    console.log("TESTCASE:Back_To_List_Navigate_successful:success");
+}
+  catch (error) {
+    console.log('TESTCASE:Back_To_List_Navigate_successful:failure');
   }
-    
   
   finally{
     await page1.close();
