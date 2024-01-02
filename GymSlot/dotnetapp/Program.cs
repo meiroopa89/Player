f572810d9da51d4c77938eb945f142aa9921fbcf
Iamneo <techadmin@iamneo.ai>
2024-01-02 10:50:26 +0000

    Auto-save-commit-1/2/2024--4:20:33-PM

--- GymSlot/dotnetapp/Program.cs
+++ GymSlot/dotnetapp/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Slot}/{action=Book}/{id?}");
 
 app.Run();
