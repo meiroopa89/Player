a4893780a2a490579d6c66c192a47153ec4e47a1
Iamneo <techadmin@iamneo.ai>
2024-01-02 11:20:26 +0000

    Auto-save-commit-1/2/2024--4:50:33-PM

--- GymSlot/dotnetapp/TestProject/UnitTest1.cs
+++ GymSlot/dotnetapp/TestProject/UnitTest1.cs
@@ -1,15 +1,62 @@
-namespace TestProject;
+using System.Collections.Generic;
+using System.Linq;
+using dotnetapp.Controllers;
+using dotnetapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 
-public class Tests
+
+namespace dotnetapp.TestProject
 {
+
+public class TestProject
+{
+
+        private ApplicationDbContext _context;
+        private BookingController _controller;
+        private SlotController _slotcontroller;
+
+        
     [SetUp]
     public void Setup()
     {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+            _context = new ApplicationDbContext(options);
+            _context.Database.EnsureCreated();
+
+            _controller = new BookingController(_context);
+            _slotcontroller = new SlotController(_context);
+
+            
     }
 
+    [TearDown]
+        public void TearDown()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+
     [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+        public void Index_ReturnsViewWithBookings()
+        {
+            var bookings = new List<Booking>
+            {
+                new Booking { Slot = new Slot() },
+                new Booking { Slot = new Slot() }
+            };
+            _context.Bookings.AddRange(bookings);
+            _context.SaveChanges();
+
+            var result = _controller.Index() as ViewResult;
+            Console.WriteLine(bookings.Count);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(bookings.Count, (result.Model as List<Booking>)?.Count);
+        }
+
+     
+}
 }
