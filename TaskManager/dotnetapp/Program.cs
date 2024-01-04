c3752ea27f6033082a705502ee4788d44eaf9622
Iamneo <techadmin@iamneo.ai>
2024-01-04 05:31:47 +0000

    Auto-save-commit-1/4/2024--11:01:55-AM

--- TaskManager/dotnetapp/Program.cs
+++ TaskManager/dotnetapp/Program.cs
@@ -1,3 +1,41 @@
+// using dotnetapp.Models;
+// using Microsoft.EntityFrameworkCore;
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+// builder.Services.AddControllersWithViews();
+
+
+// builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("Task")));
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Home/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
+
+// app.UseHttpsRedirection();
+// app.UseStaticFiles();
+
+// app.UseRouting();
+
+// app.UseAuthorization();
+
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=TaskList}/{action=Index}/{id?}"),
+//     pattern: "{controller=TaskItem}/{action=Index}/{id?}");
+
+// app.Run();
+
+
+
 using dotnetapp.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,7 +43,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Task")));
@@ -16,7 +53,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -28,8 +64,17 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "taskListRoute",
+    pattern: "TaskList/{action=Index}/{id?}",
+    defaults: new { controller = "TaskList" });
+
+app.MapControllerRoute(
+    name: "taskItemRoute",
+    pattern: "TaskItem/{action=Index}/{listId?}/{id?}",
+    defaults: new { controller = "TaskItem" });
+
+app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=TaskList}/{action=Index}/{id?}"),
-    pattern: "{controller=TaskItem}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
