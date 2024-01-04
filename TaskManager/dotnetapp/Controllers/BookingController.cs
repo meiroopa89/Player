588fd75867ba764dd73fbcafe7d872dc47d2a56b
Iamneo <techadmin@iamneo.ai>
2024-01-04 17:54:42 +0000

    Auto-save-commit-1/4/2024--11:24:50-PM

--- TaskManager/dotnetapp/Controllers/BookingController.cs
+++ TaskManager/dotnetapp/Controllers/BookingController.cs
@@ -6,7 +6,173 @@ using System.Threading.Tasks;
 
 namespace dotnetapp.Controllers
 {
-    public class BookingController : Controller
+//     public class BookingController : Controller
+// {
+//     private readonly ApplicationDbContext _context;
+
+//     public BookingController(ApplicationDbContext context)
+//     {
+//         _context = context;
+//     }
+
+//     // GET: /Booking/Delete/5
+//     public IActionResult Delete(int? id)
+//     {
+//         if (id == null)
+//         {
+//             return NotFound();
+//         }
+
+//         var booking = _context.Bookings.FirstOrDefault(m => m.BookingId == id);
+//         if (booking == null)
+//         {
+//             return NotFound();
+//         }
+
+//         return View(booking);
+//     }
+
+//     // POST: /Booking/Delete/5
+//     [HttpPost, ActionName("Delete")]
+//     [ValidateAntiForgeryToken]
+//     public IActionResult DeleteConfirmed(int id)
+//     {
+//         var booking = _context.Bookings.Find(id);
+//         if (booking == null)
+//         {
+//             return NotFound();
+//         }
+
+//         _context.Bookings.Remove(booking);
+//         _context.SaveChanges();
+
+//         return RedirectToAction(nameof(Index));
+//     }
+
+//     // GET: /Booking/Create
+//     public IActionResult Create()
+//     {
+//         return View();
+//     }
+
+//     // POST: /Booking/Create
+//     [HttpPost]
+//     [ValidateAntiForgeryToken]
+//     public IActionResult Create(Booking booking)
+//     {
+//         if (ModelState.IsValid)
+//         {
+//             // Save the booking to the database
+//             _context.Bookings.Add(booking);
+//             _context.SaveChanges();
+            
+//             return RedirectToAction(nameof(Index));
+//         }
+        
+//         return View(booking);
+//     }
+
+//     public IActionResult Create(Vehicle vehicle)
+// {
+//     if (ModelState.IsValid)
+//     {
+//         // Logic to save the vehicle details to the database goes here
+
+//         // Redirect to the Booking/Create action after successful submission
+//         return RedirectToAction("Create", "Booking"); // Replace "Create" with the appropriate action in your BookingController
+//     }
+
+//     return View(vehicle);
+// }
+
+//     // GET: /Booking/Details/5
+//     public IActionResult Details(int? id)
+//     {
+//         if (id == null)
+//         {
+//             return NotFound();
+//         }
+
+//         var booking = _context.Bookings.FirstOrDefault(m => m.BookingId == id);
+//         if (booking == null)
+//         {
+//             return NotFound();
+//         }
+
+//         return View(booking);
+//     }
+
+//     public IActionResult Edit(int? id)
+//     {
+//         if (id == null)
+//         {
+//             return NotFound();
+//         }
+
+//         var booking = _context.Bookings.Find(id);
+//         if (booking == null)
+//         {
+//             return NotFound();
+//         }
+
+//         return View(booking);
+//     }
+
+//     // POST: /Booking/Edit/5
+//     [HttpPost]
+//     [ValidateAntiForgeryToken]
+//     public IActionResult Edit(int id, Booking booking)
+//     {
+//         if (id != booking.BookingId)
+//         {
+//             return NotFound();
+//         }
+
+//         if (ModelState.IsValid)
+//         {
+//             try
+//             {
+//                 _context.Update(booking);
+//                 _context.SaveChanges();
+//             }
+//             catch (DbUpdateConcurrencyException)
+//             {
+//                 if (!BookingExists(booking.BookingId))
+//                 {
+//                     return NotFound();
+//                 }
+//                 else
+//                 {
+//                     throw;
+//                 }
+//             }
+//             return RedirectToAction(nameof(Index));
+//         }
+//         return View(booking);
+//     }
+
+//     // GET: /Booking/Index
+//     public IActionResult Index()
+//     {
+//         var bookings = _context.Bookings.ToList();
+//         return View(bookings);
+//     }
+
+//     // Other actions like Create, Details, Delete, etc.
+
+//     private bool BookingExists(int id)
+//     {
+//         return _context.Bookings.Any(e => e.BookingId == id);
+//     }
+
+// }
+
+// }
+
+
+
+
+public class BookingController : Controller
 {
     private readonly ApplicationDbContext _context;
 
@@ -15,7 +181,47 @@ namespace dotnetapp.Controllers
         _context = context;
     }
 
-    // GET: /Booking/Delete/5
+    // Existing actions...
+
+    // GET: Booking/Edit/5
+    public IActionResult Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var booking = _context.Bookings.FirstOrDefault(m => m.BookingId == id);
+        if (booking == null)
+        {
+            return NotFound();
+        }
+
+        return View(booking);
+    }
+
+    // POST: Booking/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Booking booking)
+    {
+        if (id != booking.BookingId)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(booking);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Booking");
+        }
+
+        return View(booking);
+    }
+
+    // GET: Booking/Delete/5
     public IActionResult Delete(int? id)
     {
         if (id == null)
@@ -32,7 +238,7 @@ namespace dotnetapp.Controllers
         return View(booking);
     }
 
-    // POST: /Booking/Delete/5
+    // POST: Booking/Delete/5
     [HttpPost, ActionName("Delete")]
     [ValidateAntiForgeryToken]
     public IActionResult DeleteConfirmed(int id)
@@ -46,9 +252,9 @@ namespace dotnetapp.Controllers
         _context.Bookings.Remove(booking);
         _context.SaveChanges();
 
-        return RedirectToAction(nameof(Index));
+       return RedirectToAction("Index", "Booking");
     }
-
+    
     // GET: /Booking/Create
     public IActionResult Create()
     {
@@ -66,7 +272,8 @@ namespace dotnetapp.Controllers
             _context.Bookings.Add(booking);
             _context.SaveChanges();
             
-            return RedirectToAction(nameof(Index));
+            // return RedirectToAction(nameof(Index));
+              return RedirectToAction("Index", "Booking");
         }
         
         return View(booking);
@@ -84,86 +291,6 @@ namespace dotnetapp.Controllers
 
     return View(vehicle);
 }
-
-    // GET: /Booking/Details/5
-    public IActionResult Details(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var booking = _context.Bookings.FirstOrDefault(m => m.BookingId == id);
-        if (booking == null)
-        {
-            return NotFound();
-        }
-
-        return View(booking);
-    }
-
-    public IActionResult Edit(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var booking = _context.Bookings.Find(id);
-        if (booking == null)
-        {
-            return NotFound();
-        }
-
-        return View(booking);
-    }
-
-    // POST: /Booking/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public IActionResult Edit(int id, Booking booking)
-    {
-        if (id != booking.BookingId)
-        {
-            return NotFound();
-        }
-
-        if (ModelState.IsValid)
-        {
-            try
-            {
-                _context.Update(booking);
-                _context.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!BookingExists(booking.BookingId))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
-        return View(booking);
-    }
-
-    // GET: /Booking/Index
-    public IActionResult Index()
-    {
-        var bookings = _context.Bookings.ToList();
-        return View(bookings);
-    }
-
-    // Other actions like Create, Details, Delete, etc.
-
-    private bool BookingExists(int id)
-    {
-        return _context.Bookings.Any(e => e.BookingId == id);
-    }
 
 }
 
