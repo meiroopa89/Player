b09ae15c4f4b3fea65825262d4ea8c648a9c8e36
neojarvis <techadmin@iamneo.ai>
2024-01-06 08:44:03 +0000

    Auto-save-commit-1/6/2024--2:14:11-PM

--- Employee/TestProject/UnitTest1.cs
+++ Employee/TestProject/UnitTest1.cs
@@ -81,5 +81,94 @@ public class Tests
             Assert.AreEqual(typeof(int), expectedType, "Property TaskID in VerificationTask class is not of type int");
         }
 
-        VerificationDetails
+        
+        [Test]
+        public void VerificationTask_VerificationDetails_PropertyExists_ReturnExpectedDataTypes_string()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Models.VerificationTask";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type VerificationTaskType = assembly.GetType(typeName);
+            PropertyInfo propertyInfo = VerificationTaskType.GetProperty("VerificationDetails");
+            Assert.IsNotNull(propertyInfo, "Property VerificationDetails does not exist in VerificationTask class");
+            Type expectedType = propertyInfo.PropertyType;
+            Assert.AreEqual(typeof(string), expectedType, "Property VerificationDetails in VerificationTask class is not of type string");
+        }
+
+        [Test]
+        public void VerificationTask_Status_PropertyExists_ReturnExpectedDataTypes_string()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Models.VerificationTask";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type VerificationTaskType = assembly.GetType(typeName);
+            PropertyInfo propertyInfo = VerificationTaskType.GetProperty("Status");
+            Assert.IsNotNull(propertyInfo, "Property Status does not exist in VerificationTask class");
+            Type expectedType = propertyInfo.PropertyType;
+            Assert.AreEqual(typeof(string), expectedType, "Property Status in VerificationTask class is not of type string");
+        }
+
+        [Test]
+        public void CandidateController_Controllers_ClassExists()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Controllers.CandidateController";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type CandidateControllerType = assembly.GetType(typeName);
+            Assert.IsNotNull(CandidateControllerType);
+        }
+
+        [Test]
+        public void VerificationController_Controllers_ClassExists()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Controllers.VerificationController";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type VerificationControllerType = assembly.GetType(typeName);
+            Assert.IsNotNull(VerificationControllerType);
+        }
+
+        // [Test]
+        // public void CandidateController_Create_MethodExists()
+        // {
+        //     string assemblyName = "dotnetapp";
+        //     string typeName = "dotnetapp.Controllers.CandidateController";
+        //     Assembly assembly = Assembly.Load(assemblyName);
+        //     Type CandidateControllerType = assembly.GetType(typeName);
+        //     MethodInfo methodInfo = CandidateControllerType.GetMethod("Create");
+        //     Assert.IsNotNull(methodInfo, "Method Create does not exist in CandidateController class");
+        // }
+
+        // [Test]
+        // public void VerificationController_Create_MethodExists()
+        // {
+        //     string assemblyName = "dotnetapp";
+        //     string typeName = "dotnetapp.Controllers.VerificationController";
+        //     Assembly assembly = Assembly.Load(assemblyName);
+        //     Type VerificationControllerType = assembly.GetType(typeName);
+        //     MethodInfo methodInfo = VerificationControllerType.GetMethod("Create");
+        //     Assert.IsNotNull(methodInfo, "Method Create does not exist in VerificationController class");
+        // }
+
+        [Test]
+        public void VerificationController_Index_MethodExists()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Controllers.VerificationController";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type VerificationControllerType = assembly.GetType(typeName);
+            MethodInfo methodInfo = VerificationControllerType.GetMethod("Index");
+            Assert.IsNotNull(methodInfo, "Method Index does not exist in VerificationController class");
+        }
+
+        // [Test]
+        // public void VerificationController_Index_MethodExists()
+        // {
+        //     string assemblyName = "dotnetapp";
+        //     string typeName = "dotnetapp.Controllers.VerificationController";
+        //     Assembly assembly = Assembly.Load(assemblyName);
+        //     Type VerificationControllerType = assembly.GetType(typeName);
+        //     MethodInfo methodInfo = VerificationControllerType.GetMethod("Index");
+        //     Assert.IsNotNull(methodInfo, "Method Index does not exist in VerificationController class");
+        // }
 }
