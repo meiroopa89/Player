ab73223c8da4002b4f49fd933e8d2bb0ab1c1650
neojarvis <techadmin@iamneo.ai>
2024-01-14 08:15:57 +0000

    Auto-save-commit-1/14/2024--1:46:06-PM

--- Expense/dotnetapp/Controllers/CategoryController.cs
+++ Expense/dotnetapp/Controllers/CategoryController.cs
@@ -5,35 +5,43 @@ namespace dotnetapp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CategoryController : ControllerBase
+    
+    public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoryController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
-
-        public CategoryController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/Category/Create
-        [HttpGet("Create")]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: api/Category/Create
-        [HttpPost("Create")]
-        public IActionResult Create([FromBody] Category category)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _context.Categories.Add(category);
-            _context.SaveChanges();
-
-            return CreatedAtAction("GetCategory", new { id = category.Id }, category);
-        }
+        _context = context;
     }
+
+    // GET: Category/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Category/Create
+    [HttpPost]
+    public IActionResult Create([FromBody] Category category)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        _context.Categories.Add(category);
+        _context.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
+
+    // GET: Category/Index
+    public IActionResult Index()
+    {
+        var categories = _context.Categories.ToList();
+        return View(categories);
+    }
+}
+
 }
--- Expense/dotnetapp/Program.cs
+++ Expense/dotnetapp/Program.cs
@@ -9,14 +9,12 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Expense")));
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -29,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Category}/{action=Create}/{id?}");
 
 app.Run();
