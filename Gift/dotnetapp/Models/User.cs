b6588ab148ce9eacd5fe9b683b6a08e3cc112734
neojarvis <techadmin@iamneo.ai>
2024-02-12 05:11:28 +0000

    Auto-save-commit-12/02/2024--10:41:19

--- Gift/dotnetapp/Models/User.cs
+++ Gift/dotnetapp/Models/User.cs
@@ -6,7 +6,6 @@ namespace dotnetapp.Models
 public class User
 {
     [Key]
-    // [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long UserId { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
