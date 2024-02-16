ae23bfa1c7777eef1c960e80fda12548209cafd9
neojarvis <techadmin@iamneo.ai>
2024-02-16 04:31:04 +0000

    Auto-save-commit-16/02/2024--10:01:00

--- Gift/TestProject/UnitTest1.cs
+++ Gift/TestProject/UnitTest1.cs
@@ -17,7 +17,11 @@ public class Tests
     public void Setup()
     {
          _httpClient = new HttpClient();
-        _httpClient.BaseAddress = new Uri("https://localhost:7266"); 
+        // _httpClient.BaseAddress = new Uri("https://localhost:7266"); 
+        // _httpClient.BaseAddress = new Uri("https://api.example.com/"); 
+        _httpClient.BaseAddress = new Uri("https://8080-bfdeeddcedfabcfacbdcbaeadbebabcdebdca.premiumproject.examly.io/"); 
+
+
     }
 
     // [Test]
@@ -26,23 +30,48 @@ public class Tests
     //     Assert.Pass();
     // }
 
+    // [Test, Order(1)]
+    // public async Task Backend_TestRegisterUser()
+    // {
+    //     string uniqueId = Guid.NewGuid().ToString();
+
+    //     // Generate a unique userName based on a timestamp
+    //     string uniqueUsername = $"abcd_{uniqueId}";
+    //     string uniqueEmail = $"abcd{uniqueId}@gmail.com";
+
+
+    //     string requestBody = $"{{\"password\": \"abc@123A\",\"email\": \"{uniqueEmail}\"}}";
+    //     HttpResponseMessage response = await _httpClient.PostAsync("/api/login", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+    //     Console.WriteLine(response.Content);
+    //     string responseString = await response.Content.ReadAsStringAsync();
+
+    //     Console.WriteLine(response.StatusCode);
+    //     Console.WriteLine(responseString);
+    //     // Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    // }
+
     [Test, Order(1)]
-    public async Task Backend_TestRegisterUser()
+    public async Task Backend_TestLoginAdmin()
     {
         string uniqueId = Guid.NewGuid().ToString();
 
-        // Generate a unique userName based on a timestamp
-        string uniqueUsername = $"abcd_{uniqueId}";
-        string uniqueEmail = $"abcd{uniqueId}@gmail.com";
-
-
-        string requestBody = $"{{\"password\": \"abc@123A\",\"email\": \"{uniqueEmail}\"}}";
-        HttpResponseMessage response = await _httpClient.PostAsync("/user/login", new StringContent(requestBody, Encoding.UTF8, "application/json"));
-        Console.WriteLine(response.Content);
-        string responseString = await response.Content.ReadAsStringAsync();
-
-        Console.WriteLine(response.StatusCode);
-        Console.WriteLine(responseString);
+        string uniqueusername = $"abcd_{uniqueId}";
+        string uniquepassword = $"abcdA{uniqueId}@123";
+        string uniquerole = "admin";
+        string requestBody = $"{{\"Username\" : \"{uniqueusername}\",\"Password\" : \"{uniquepassword}\",\"Role\" : \"{uniquerole}\" }}";
+        HttpResponseMessage response = await _httpClient.PostAsync("/api/register", new StringContent(requestBody, Encoding.UTF8, "application/json"));
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        string requestBody1 = $"{{\"Username\" : \"{uniqueemail}\",\"Password\" : \"{uniquepassword}\"}}";
+        HttpResponseMessage response1 = await _httpClient.PostAsync("/api/login", new StringContent(requestBody1, Encoding.UTF8, "application/json"));
+        Assert.AreEqual(HttpStatusCode.OK, response1.StatusCode);
+        string responseBody = await response1.Content.ReadAsStringAsync();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Cleanup or additional teardown logic if needed.
+        _httpClient.Dispose();
     }
 }
