e2cca4c990bb2198b9616499178d4360a86c2be4
neojarvis <techadmin@iamneo.ai>
2024-03-06 00:37:42 +0000

    Auto-save-commit-06/03/2024--06:07:39

--- blog/TestProject/UnitTest1.cs
+++ blog/TestProject/UnitTest1.cs
@@ -20,5 +20,112 @@ public class Tests
             Assert.IsNotNull(postType);
         }
 
+        [Test]
+        public void Post_Id_PropertyExists_ReturnExpectedDataTypes_int()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Model.Post";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type PostType = assembly.GetType(typeName);
+            PropertyInfo propertyInfo = PostType.GetProperty("Id");
+            Assert.IsNotNull(propertyInfo, "Property Id does not exist in Post class");
+            Type expectedType = propertyInfo.PropertyType;
+            Assert.AreEqual(typeof(int), expectedType, "Property Id in Post class is not of type int");
+        }
+
+        [Test]
+        public void Post_Title_PropertyExists_ReturnExpectedDataTypes_string()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Model.Post";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type PostType = assembly.GetType(typeName);
+            PropertyInfo propertyInfo = PostType.GetProperty("Title");
+            Assert.IsNotNull(propertyInfo, "Property Title does not exist in Post class");
+            Type expectedType = propertyInfo.PropertyType;
+            Assert.AreEqual(typeof(string), expectedType, "Property Title in Post class is not of type string");
+        }
+
+        [Test]
+        public void Post_Content_PropertyExists_ReturnExpectedDataTypes_string()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Model.Post";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type PostType = assembly.GetType(typeName);
+            PropertyInfo propertyInfo = PostType.GetProperty("Content");
+            Assert.IsNotNull(propertyInfo, "Property Content does not exist in Post class");
+            Type expectedType = propertyInfo.PropertyType;
+            Assert.AreEqual(typeof(string), expectedType, "Property Content in Post class is not of type string");
+        }
+
+        [Test]
+        public void Comment_Model_ClassExists()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Model.Comment";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type CommentType = assembly.GetType(typeName);
+            Assert.IsNotNull(CommentType);
+        }
+
+        [Test]
+        public void Comment_Id_PropertyExists_ReturnExpectedDataTypes_int()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Model.Comment";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type CommentType = assembly.GetType(typeName);
+            PropertyInfo propertyInfo = CommentType.GetProperty("Id");
+            Assert.IsNotNull(propertyInfo, "Property Id does not exist in Comment class");
+            Type expectedType = propertyInfo.PropertyType;
+            Assert.AreEqual(typeof(int), expectedType, "Property Id in Post class is not of type int");
+        }
+
+        [Test]
+        public void Comment_Text_PropertyExists_ReturnExpectedDataTypes_string()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Model.Comment";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type CommentType = assembly.GetType(typeName);
+            PropertyInfo propertyInfo = CommentType.GetProperty("Text");
+            Assert.IsNotNull(propertyInfo, "Property Text does not exist in Comment class");
+            Type expectedType = propertyInfo.PropertyType;
+            Assert.AreEqual(typeof(string), expectedType, "Property Text in Comment class is not of type string");
+        }
+
+        [Test]
+        public void PostController_Controllers_ClassExists()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Controllers.PostController";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type PostControllerType = assembly.GetType(typeName);
+            Assert.IsNotNull(PostControllerType);
+        }
+
+        [Test]
+        public void CommentController_Controllers_ClassExists()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Controllers.CommentController";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type CommentControllerType = assembly.GetType(typeName);
+            Assert.IsNotNull(CommentControllerType);
+        }
+
+        [Test]
+        public void PostController_AddPost_MethodExists()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Controllers.PostController";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type PostControllerType = assembly.GetType(typeName);
+            MethodInfo methodInfo = PostControllerType.GetMethod("AddPost", Type.EmptyTypes);
+            Assert.IsNotNull(methodInfo, "Method AddPost does not exist in PostController class");
+        }
+
+
 }
 }
--- blog/dotnetapp/Controllers/CommentController.cs
+++ blog/dotnetapp/Controllers/CommentController.cs
@@ -1,76 +1,10 @@
-// // CommentController.cs
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// using System.Linq;
-// using dotnetapp.Services;
-
-
-// [Route("api/posts/{postId}/comments")]
-// [ApiController]
-// public class CommentController : ControllerBase
-// {
-//     private readonly ICommentService _commentService;
-
-//     public CommentController(ICommentService commentService)
-//     {
-//         _commentService = commentService;
-//     }
-
-//     [HttpGet]
-//     public IActionResult GetComments(int postId)
-//     {
-//         var comments = _commentService.GetAllComments(postId);
-//         return Ok(comments);
-//     }
-
-//     [HttpGet("{commentId}")]
-//     public IActionResult GetComment(int postId, int commentId)
-//     {
-//         var comment = _commentService.GetComment(commentId);
-//         if (comment == null)
-//             return NotFound();
-
-//         return Ok(comment);
-//     }
-
-//     [HttpPost]
-//     public IActionResult CreateComment(int postId, [FromBody] Comment comment)
-//     {
-//         if (!ModelState.IsValid)
-//             return BadRequest(ModelState);
-
-//         _commentService.SaveComment(postId, comment);
-//         return Created($"/api/posts/{postId}/comments/{comment.Id}", comment);
-//     }
-
-//     [HttpPut("{commentId}")]
-//     public IActionResult UpdateComment(int postId, int commentId, [FromBody] Comment updatedComment)
-//     {
-//         var existingComment = _commentService.GetComment(commentId);
-//         if (existingComment == null)
-//             return NotFound();
-
-//         _commentService.UpdateComment(updatedComment);
-//         return NoContent();
-//     }
-
-//     [HttpDelete("{commentId}")]
-//     public IActionResult DeleteComment(int postId, int commentId)
-//     {
-//         var existingComment = _commentService.GetComment(commentId);
-//         if (existingComment == null)
-//             return NotFound();
-
-//         _commentService.DeleteComment(commentId);
-//         return NoContent();
-//     }
-// }
-
 // CommentController.cs
 using Microsoft.AspNetCore.Mvc;
 using dotnetapp.Model;
 using dotnetapp.Services;
 
+namespace dotnetapp.Controllers
+{
 [Route("api/posts/{postId}/comments")]
 [ApiController]
 public class CommentController : ControllerBase
@@ -109,35 +43,8 @@ public class CommentController : ControllerBase
         return Created($"/api/posts/{postId}/comments/{comment.Id}", comment);
     }
 
-    // [HttpPut("{commentId}")]
-    // public IActionResult UpdateComment(int postId, int commentId, [FromBody] Comment updatedComment)
-    // {
-    //     var existingComment = _commentService.GetComment(commentId);
-    //     if (existingComment == null)
-    //         return NotFound();
-
-    //     _commentService.UpdateComment(updatedComment);
-    //     return NoContent();
-    // }
-
-
     
-    // [HttpPut("{commentId}")]
-    // public IActionResult UpdateComment(int postId, int commentId, [FromBody] Comment updatedComment)
-    // {
-    //     Console.WriteLine(postId);
-    //     Console.WriteLine(commentId);
-    //     var existingComment = _commentService.GetComment(commentId);
-    //     Console.WriteLine(existingComment);
-    //     if (existingComment == null)
-    //         return NotFound();
-
-    //     _commentService.UpdateComment(updatedComment);
-    //     Console.WriteLine(updatedComment);
-
-    //     return NoContent();
-    // }
- [HttpPut("{commentId}")]
+    [HttpPut("{commentId}")]
     public IActionResult UpdateComment(int postId, int commentId, [FromBody] Comment updatedComment)
     {
         var result = _commentService.UpdateComment(commentId, updatedComment);
@@ -156,4 +63,5 @@ public class CommentController : ControllerBase
         _commentService.DeleteComment(commentId);
         return NoContent();
     }
+}
 }
