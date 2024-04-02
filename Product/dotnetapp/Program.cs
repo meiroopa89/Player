2bc64c698b1ce24391e9404bcd34815ff74f234f
neojarvis <techadmin@iamneo.ai>
2024-04-02 07:25:08 +0000

    Auto-save-commit-02/04/2024--12:55:06

--- Product/dotnetapp/Program.cs
+++ Product/dotnetapp/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using dotnetapp.Models;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
@@ -22,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Product}/{action=View}/{id?}");
 
 app.Run();
