eece6e34ec6cf99aca9d968df5d476cd210b0b09
neojarvis <techadmin@iamneo.ai>
2024-04-04 09:54:23 +0000

    Auto-save-commit-04/04/2024--15:24:21

--- Receipe/angularapp/src/app/app-routing.module.ts
+++ Receipe/angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'addNewRecipe', component: RecipeFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Receipe/angularapp/src/app/app.module.ts
+++ Receipe/angularapp/src/app/app.module.ts
@@ -5,14 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RecipeForm } from './receipe-form-component/receipe-form.component';}
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
+import { HeaderComponent } from './header/header.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeForm
+    RecipeFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
