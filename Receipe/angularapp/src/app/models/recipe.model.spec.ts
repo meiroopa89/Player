98b8a31352108fd29389ac315b7be1d06f40b803
neojarvis <techadmin@iamneo.ai>
2024-04-05 06:29:18 +0000

    Auto-save-commit-05/04/2024--11:59:17

---
+++ Receipe/angularapp/src/app/models/recipe.model.spec.ts
@@ -0,0 +1,71 @@
+import { Recipe } from '../models/recipe.model';
+
+describe('Recipe', () => {
+  fit('RecipeModel_should create an instance', () => {
+    // Create a factory function to create instances of Recipe
+    function createRecipe(recipeId: number, name: string, description: string, ingredients: string, instructions: string, author: string): Recipe {
+      return {
+        recipeId,
+        name,
+        description,
+        ingredients,
+        instructions,
+        author
+      };
+    }
+
+    // Use the factory function to create an instance of Recipe
+    const recipe: Recipe = createRecipe(
+      1,
+      'Test Recipe',
+      'Test Description',
+      'Test Ingredients',
+      'Test Instructions',
+      'Test Author'
+    );
+
+    // Assert that the instance is created successfully
+    expect(recipe).toBeDefined();
+    // This line will cause a compilation error and should be removed
+    // expect(recipe instanceof Recipe).toBeTruthy();
+  });
+
+  fit('RecipeModel_should update property values using setters', () => {
+    // Similar setup as above
+    function createRecipe(recipeId: number, name: string, description: string, ingredients: string, instructions: string, author: string): Recipe {
+      return {
+        recipeId,
+        name,
+        description,
+        ingredients,
+        instructions,
+        author
+      };
+    }
+
+    const recipe: Recipe = createRecipe(
+      1,
+      'Test Recipe',
+      'Test Description',
+      'Test Ingredients',
+      'Test Instructions',
+      'Test Author'
+    );
+
+    // Assert that the instance is created successfully
+    expect(recipe).toBeTruthy();
+    // Assertions for property values and data types remain the same
+    expect(recipe.recipeId).toBeTruthy();
+    expect(recipe.name).toBeTruthy();
+    expect(recipe.description).toBeTruthy();
+    expect(recipe.ingredients).toBeTruthy();
+    expect(recipe.instructions).toBeTruthy();
+    expect(recipe.author).toBeTruthy();
+    expect(typeof recipe.recipeId).toEqual('number');
+    expect(typeof recipe.name).toEqual('string');
+    expect(typeof recipe.description).toEqual('string');
+    expect(typeof recipe.ingredients).toEqual('string');
+    expect(typeof recipe.instructions).toEqual('string');
+    expect(typeof recipe.author).toEqual('string');
+  });
+});
--- Receipe/angularapp/src/app/services/recipe.service.spec.ts
+++ Receipe/angularapp/src/app/services/recipe.service.spec.ts
@@ -1,17 +1,75 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+
+import { Recipe } from '../models/recipe.model';
 import { RecipeService } from './recipe.service';
-
-
 
 describe('RecipeService', () => {
   let service: RecipeService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [RecipeService],
+    });
     service = TestBed.inject(RecipeService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('RecipeService_should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('RecipeService_should have addRecipe method', () => {
+    expect(service.addRecipe).toBeTruthy();
+  });
+
+  it('RecipeService_should have getRecipes method', () => {
+    expect(service.getRecipes).toBeTruthy();
+  });
+
+  fit('RecipeService_should_add_a_recipe and return it', () => {
+    const mockRecipe: Recipe = {
+      recipeId: 1,
+      name: 'Test Recipe',
+      description: 'Test Description',
+      ingredients: 'Test Ingredients',
+      instructions: 'Test Instructions',
+      author: 'Test Author'
+    };
+
+    service.addRecipe(mockRecipe).subscribe((recipe) => {
+      expect(recipe).toEqual(mockRecipe);
+    });
+
+    const req = httpTestingController.expectOne(`${service['apiUrl']}api/Recipe`);
+    expect(req.request.method).toBe('POST');
+    req.flush(mockRecipe);
+  });
+
+  fit('RecipeService_should_get_recipes', () => {
+    const mockRecipes: Recipe[] = [
+      {
+        recipeId: 1,
+        name: 'Test Recipe 1',
+        description: 'Test Description',
+        ingredients: 'Test Ingredients',
+        instructions: 'Test Instructions',
+        author: 'Test Author'
+      }
+    ];
+
+    service.getRecipes().subscribe((recipes) => {
+      expect(recipes).toEqual(mockRecipes);
+    });
+
+    const req = httpTestingController.expectOne(`${service['apiUrl']}api/Recipe`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockRecipes);
   });
 });
