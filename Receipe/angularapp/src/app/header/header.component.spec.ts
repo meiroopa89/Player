589b4f9398ff3166afb173b77a5616746ac00603
neojarvis <techadmin@iamneo.ai>
2024-04-05 06:07:22 +0000

    Manual-save-commit-05/04/2024--11:37:20

--- Receipe/angularapp/src/app/header/header.component.spec.ts
+++ Receipe/angularapp/src/app/header/header.component.spec.ts
@@ -1,25 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
+    let router: Router;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [HeaderComponent],
+            imports: [RouterTestingModule],
+        }).compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        router = TestBed.inject(Router);
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    fit('should create HeaderComponent', () => {
+        expect(component).toBeTruthy();
+    });
+
+    fit('HeaderComponent_should navigate to Add New Recipe', () => {
+        spyOn(router, 'navigate');
+        component.navigateToAddRecipe();
+        expect(router.navigate).toHaveBeenCalledWith(['/addNewRecipe']);
+    });
+
+    fit('HeaderComponent_should navigate to View Recipe', () => {
+        spyOn(router, 'navigate');
+        component.navigateToViewRecipes();
+        expect(router.navigate).toHaveBeenCalledWith(['/viewRecipes']);
+    });
+
+    fit('HeaderComponent_should have Recipe Sharing Platform as the brand', () => {
+        const brandElement: HTMLElement = fixture.nativeElement.querySelector('h1');
+        expect(brandElement.textContent).toContain('Recipe Sharing Platform');
+    });
+
+    fit('HeaderComponent_should have a link with text View Recipe', () => {
+        const navItems: NodeListOf<HTMLElement> = fixture.nativeElement.querySelectorAll('a');
+        const viewRecipeLink: HTMLElement = navItems[navItems.length - 1];
+        expect(viewRecipeLink.textContent).toContain('View Recipe');
+    });
 });
