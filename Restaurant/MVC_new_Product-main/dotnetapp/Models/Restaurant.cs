2ac75d7e4685b47f86fd2daee49f6a90e82934a1
neojarvis <techadmin@iamneo.ai>
2024-04-15 13:17:22 +0000

    Auto-save-commit-15/04/2024--18:47:16

--- Restaurant/MVC_new_Product-main/dotnetapp/Models/Restaurant.cs
+++ Restaurant/MVC_new_Product-main/dotnetapp/Models/Restaurant.cs
@@ -1,15 +1,28 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace dotnetapp.Models
 {
     public class Restaurant
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Location is required")]
         public string Location { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; } // Additional property for describing the restaurant
+
+        [Required(ErrorMessage = "Cuisine type is required")]
         public string CuisineType { get; set; } // Property to specify the type of cuisine served
-        public TimeSpan OpeningTime { get; set; } // Property to store the opening time
-        public TimeSpan ClosingTime { get; set; } // Property to store the closing time
+
+        [Required(ErrorMessage = "Opening time is required")]
+        public TimeSpan? OpeningTime { get; set; } // Property to store the opening time
+
+        [Required(ErrorMessage = "Closing time is required")]
+        public TimeSpan? ClosingTime { get; set; } // Property to store the closing time
     }
 }
