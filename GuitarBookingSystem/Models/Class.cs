d128bd83ae815ae1ed1b5b859df51e5e7e87c991
neojarvis <techadmin@iamneo.ai>
2024-04-23 12:31:53 +0000

    Auto-save-commit-23/04/2024--18:01:50

--- GuitarBookingSystem/Models/Class.cs
+++ GuitarBookingSystem/Models/Class.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GuitarBookingSystem.Models
+
 {
     public class Class
     {
+        [Required(ErrorMessage = "ClassID is required")]
         public int ClassID { get; set; }
+
+        [Required(ErrorMessage = "StartTime is required")]
         public DateTime StartTime { get; set; }
+
+        [Required(ErrorMessage = "EndTime is required")]
         public DateTime EndTime { get; set; }
+
+        [Required(ErrorMessage = "Capacity is required")]
         public int Capacity { get; set; }
+
+        [Required(ErrorMessage = "Students is required")]
         public ICollection<Student> Students { get; set; }
     }
-}
+    }
--- GuitarBookingSystem/Models/Student.cs
+++ GuitarBookingSystem/Models/Student.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GuitarBookingSystem.Models
 {
-   public class Student
+    public class Student
     {
+        [Required(ErrorMessage = "StudentID is required")]
         public int StudentID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "ClassID is required")]
         public int ClassID { get; set; }
+
         public Class Class { get; set; }
     }
 }
