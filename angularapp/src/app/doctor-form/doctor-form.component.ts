dd6e87e77af0eebda492578b3802809727878880
neojarvis <techadmin@iamneo.ai>
2024-05-07 06:03:46 +0000

    Auto-save-commit-07/05/2024--11:33:45

--- angularapp/src/app/doctor-form/doctor-form.component.ts
+++ angularapp/src/app/doctor-form/doctor-form.component.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
 import { Doctor } from '../models/doctor.model';
-import { DoctorService } from '../services/doctor.service'; // Corrected import statement
+import { DoctorService } from '../services/doctor.service';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-doctor-form',
   templateUrl: './doctor-form.component.html',
   styleUrls: ['./doctor-form.component.css']
 })
-
 export class DoctorFormComponent {
   newDoctor: Doctor = {
     id: 0,
-    name: '',
-    description: '',
-    ingredients: '',
-    instructions: '',
-    author: ''
-  } as any;
-  
+    firstName: '',
+    lastName: '',
+    specialization: '',
+    phoneNumber: '',
+    email: '',
+    address: ''
+  }; // Initialize newDoctor with empty fields
+
   formSubmitted = false; // Track form submission
 
   constructor(private doctorService: DoctorService, private router: Router) { }
@@ -36,13 +35,12 @@ export class DoctorFormComponent {
 
   isFieldInvalid(fieldName: string): boolean {
     const field = this.newDoctor[fieldName];
-    return !field && (this.formSubmitted || this.newDoctor[fieldName].touched);
+    return !field && this.formSubmitted; // Check if field is empty and form is submitted
   }
 
   isFormValid(): boolean {
-    return !this.isFieldInvalid('name') && !this.isFieldInvalid('description') &&
-      !this.isFieldInvalid('ingredients') && !this.isFieldInvalid('instructions') &&
-      !this.isFieldInvalid('author');
+    return !this.isFieldInvalid('firstName') && !this.isFieldInvalid('lastName') &&
+      !this.isFieldInvalid('specialization') && !this.isFieldInvalid('phoneNumber') &&
+      !this.isFieldInvalid('email') && !this.isFieldInvalid('address');
   }
 }
-
