4a8e40584c09987aa3cf9362d643f8ed4d11460f
neojarvis <techadmin@iamneo.ai>
2024-05-07 12:57:37 +0000

    Auto-save-commit-07/05/2024--18:27:35

---
+++ Task/dotnetapp/Models/Task.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace dotnetapp.Models
+{
+    public class Task
+    {
+        [key]
+        public int Id {get;set;}
+
+        [Required (ErrorMessage = "Name is required")]
+        public string Name {get;set;}
+        public string Description {get;set;}
+    }
+}
