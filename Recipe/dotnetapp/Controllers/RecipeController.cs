d5ec22f8b9733de6716a57f0e92f7c45a5597f90
neojarvis <techadmin@iamneo.ai>
2024-05-08 02:43:30 +0000

    Auto-save-commit-08/05/2024--08:13:28

--- Recipe/dotnetapp/Controllers/RecipeController.cs
+++ Recipe/dotnetapp/Controllers/RecipeController.cs
@@ -1,8 +1,17 @@
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
 using System.Threading.Tasks;
 using dotnetapp.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Mvc;
 
 namespace dotnetapp.Controllers
 {
+    [Route("api/[Controller]")]
     public class RecipeController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
@@ -11,6 +20,17 @@ namespace dotnetapp.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> 
+        public async Task<IActionResult<Recipe>> AddPost (Recipe recipe)
+        {
+            _context.Recipes.Add(recipe);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRecipe",new { id = Recipe.Id}, recipe);
+        } 
+
+        public async Task<IActionResult<IEnumerable<Recipe>>> GetRecipe()
+        {
+            return await _context.Recipes.ToListAsync();
+        }
     }
 }
