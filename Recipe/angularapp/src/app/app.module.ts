8c5df9a7266ae00dc26b65cb976587a38f9a7bf8
neojarvis <techadmin@iamneo.ai>
2024-05-13 05:44:26 +0000

    Auto-save-commit-13/05/2024--11:14:25

--- Recipe/angularapp/src/app/app.module.ts
+++ Recipe/angularapp/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeFormComponent,
+    RecipeListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Recipe/angularapp/src/app/services/recipe.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Recipe } from '../models/recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+
+  public apiUrl = "https://8080-bfdeeddcedfabcfacbdcbaeadbebabcdebdca.premiumproject.examly.io/";
+
+  constructor(private http: HttpClient) { }
+
+  addRecipe(Recipe: Recipe): Observable<Recipe>
+  {
+    return this.http.post<Recipe>(`${this.apiUrl}api/Recipe`, Recipe);
+  }
+  getRecipe(): Observable<Recipe[]>
+  
+}
