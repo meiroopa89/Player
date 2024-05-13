9a432b127e545f66fa4477b3d87a3f000cc7fa4e
neojarvis <techadmin@iamneo.ai>
2024-05-13 08:22:24 +0000

    Manual-save-commit-13/05/2024--13:52:23

--- Recipe/dotnetapp/Controllers/MovieController.cs
+++ Recipe/dotnetapp/Controllers/MovieController.cs
@@ -8,6 +8,8 @@ using System.Collections;
 
 namespace dotnetapp.Models
 {
+    [Route("api/movies")]
+    [ApiController]
     public class MovieController: ControllerBase
     {
         private readonly ApplicationDbContext _context;
@@ -17,12 +19,15 @@ namespace dotnetapp.Models
             _context = context;
         }
 
+        [HttpPost]
         public async Task<ActionResult<Movie>> AddMovie(Movie movie)
         {
             _context.Movies.Add(movie);
             await _context.SaveChangesAsync();
             return CreatedAtAction("GetMovies", new {movieId = movie.MovieID}, movie);
         }
+
+        [HttpGet]
 
         public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
         {
@@ -31,25 +36,27 @@ namespace dotnetapp.Models
             foreach(var movie in movies)
             {
                 var reviews = await _context.Reviews.Where(m => m.MovieID == movie.MovieID).ToListAsync();
-
-                movies.Reviews = reviews;
+                movie.Reviews = reviews;
             }
 
             return Ok(movies);
         
         }
 
+        [HttpGet("{movieId}")]
         public async Task<ActionResult<Movie>> GetMovie (int movieId)
         {
-            var Movie = _context.Movies.FindAsync(movieId);
-            var reviews = await _context.Reviews.Where(r => r.MovieID == movieId).ToListAsync();
-
-            Movie.Reviews = reviews;
+            var Movie = await _context.Movies.FindAsync(movieId);
+            
 
             if(Movie==null)
             {
                 return NotFound();
             }
+
+            var reviews = await _context.Reviews.Where(r => r.MovieID == movieId).ToListAsync();
+
+            Movie.Reviews = reviews;
 
             return Ok(Movie);
         }
--- Recipe/dotnetapp/Controllers/ReviewController.cs
+++ Recipe/dotnetapp/Controllers/ReviewController.cs
@@ -6,6 +6,8 @@ using System;
 
 namespace dotnetapp.Models
 {
+    [Route("api/reviews")]
+    [ApiController]
     public class ReviewController: ControllerBase
     {
         private readonly ApplicationDbContext _context;
@@ -14,7 +16,9 @@ namespace dotnetapp.Models
         {
             _context = context;
         }
-
+        
+        [HttpPost("{movieId}")]
+        
         public async Task<ActionResult<Review>> AddReview(int movieId, Review review)
         {
             review.MovieID = movieId;
@@ -23,6 +27,7 @@ namespace dotnetapp.Models
             return Ok();
         }
 
+        [HttpGet("{movieId}")]
         public async Task<ActionResult<Review>> GetReviews(int movieId)
         {
             var reviews = await _context.Reviews.Where(r => r.MovieID == movieId).ToListAsync();
