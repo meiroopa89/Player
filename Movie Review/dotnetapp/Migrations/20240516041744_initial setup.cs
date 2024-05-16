37c148f6d0534435a46223bb55367869010c7a6a
neojarvis <techadmin@iamneo.ai>
2024-05-16 04:19:35 +0000

    Auto-save-commit-16/05/2024--09:49:34

---
+++ Movie Review/dotnetapp/Migrations/20240516041744_initial setup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetapp.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    reviewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    movieName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    movieDirector = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    leadCast = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    movieReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    movieReviewDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    reviewComments = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.reviewId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+        }
+    }
+}
