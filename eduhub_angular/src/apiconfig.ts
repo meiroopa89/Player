a2679e85d97d0a15e1609f847b36bc25a739da6b
neojarvis <techadmin@iamneo.ai>
2024-05-21 05:40:15 +0000

    Auto-save-commit-21/05/2024--11:10:13

--- eduhub_angular/src/apiconfig.ts
+++ eduhub_angular/src/apiconfig.ts
@@ -1,1 +1,1 @@
-export const apiUrl = 'https://8080-fcebccfceabbafdecbafcaaffabb.premiumproject.examly.io';
+export const apiUrl = 'https://8080-bfdeeddcedfabcfacbdcbaeadbebabcdebdca.premiumproject.examly.io';
