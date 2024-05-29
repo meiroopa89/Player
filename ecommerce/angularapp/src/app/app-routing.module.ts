91875ffb99ba4db74d5b8fce1ec17c936b1bc29b
neojarvis <techadmin@iamneo.ai>
2024-05-29 16:10:16 +0000

    Auto-save-commit-5/29/2024--9:40:33-PM

--- ecommerce/angularapp/src/app/app-routing.module.ts
+++ ecommerce/angularapp/src/app/app-routing.module.ts
@@ -7,13 +7,14 @@ import { AddProductComponent } from './components/add-product/add-product.compon
 import { ViewProductComponent } from './components/view-product/view-product.component';
 import { EditProductComponent } from './components/edit-product/edit-product.component';
 import { DeleteConfirmComponent } from './components/delete-confirm/delete-confirm.component';
+import { CustomerViewProductComponent } from './components/customer-view-product/customer-view-product.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: RegistrationComponent },
   {path: 'admin/addProducts', component: AddProductComponent },
-  {path: 'user/viewProducts', component: ViewProductComponent},
+  {path: 'user/viewProducts', component: CustomerViewProductComponent},
   {path: 'admin/viewProducts', component: ViewProductComponent},
   { path: 'edit-product/:id', component: EditProductComponent },
   { path: 'confirmDelete/:id', component: DeleteConfirmComponent},
--- ecommerce/angularapp/src/app/components/customer-view-product/customer-view-product.component.ts
+++ ecommerce/angularapp/src/app/components/customer-view-product/customer-view-product.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-customer-view-product',
   templateUrl: './customer-view-product.component.html',
   styleUrls: ['./customer-view-product.component.css']
 })
-export class CustomerViewProductComponent implements OnInit {
+export class CustomerViewProductComponent{
 
-  constructor() { }
+  products: Product[] = [];
+
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+    }, error => {
+      console.error('Error fetching products:', error);
+    });
+  }
+
+
+  onProductUpdated(): void {
+    this.loadProducts(); // Refresh the product list
   }
 
 }
